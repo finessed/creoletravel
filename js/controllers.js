2e1fe6f7984ff08cd23bbe36f3b8a7f8bfb96d63
Delroy Finesse <finessed@users.noreply.github.com>
2018-07-05 00:40:41 +0400

    Add files via upload

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('app.controllers', [])
   
-.controller('hOMECtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
@@ -8,7 +8,7 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('cALCULATORCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('rankingCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
@@ -16,15 +16,7 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('aBOUTCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
-      
-.controller('mENUCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('trailsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
@@ -32,43 +24,11 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('tIPSCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('contactsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
 
 }])
-   
-.controller('hEREFORYOURBUSINESSCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
-   
-.controller('hEREFORYOUCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
-   
-.controller('iNVESTORRELATIONSCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
-   
-.controller('cONTACTCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
- 
+    
--- js/routes.js
+++ js/routes.js
@@ -9,32 +9,32 @@ angular.module('app.routes', [])
   $stateProvider
     
 
-      .state('tabsController.hOME', {
+      .state('tabsController.ranking', {
     url: '/page2',
     views: {
       'tab1': {
-        templateUrl: 'templates/hOME.html',
-        controller: 'hOMECtrl'
+        templateUrl: 'templates/ranking.html',
+        controller: 'rankingCtrl'
       }
     }
   })
 
-  .state('tabsController.cALCULATOR', {
+  .state('tabsController.trails', {
     url: '/page3',
     views: {
       'tab2': {
-        templateUrl: 'templates/cALCULATOR.html',
-        controller: 'cALCULATORCtrl'
+        templateUrl: 'templates/trails.html',
+        controller: 'trailsCtrl'
       }
     }
   })
 
-  .state('tabsController.aBOUT', {
+  .state('tabsController.contacts', {
     url: '/page4',
     views: {
       'tab3': {
-        templateUrl: 'templates/aBOUT.html',
-        controller: 'aBOUTCtrl'
+        templateUrl: 'templates/contacts.html',
+        controller: 'contactsCtrl'
       }
     }
   })
@@ -43,44 +43,6 @@ angular.module('app.routes', [])
     url: '/page1',
     templateUrl: 'templates/tabsController.html',
     abstract:true
-  })
-
-  .state('tIPS', {
-    url: '/page5',
-    templateUrl: 'templates/tIPS.html',
-    controller: 'tIPSCtrl'
-  })
-
-  .state('tabsController.hEREFORYOURBUSINESS', {
-    url: '/page6',
-    views: {
-      'tab1': {
-        templateUrl: 'templates/hEREFORYOURBUSINESS.html',
-        controller: 'hEREFORYOURBUSINESSCtrl'
-      }
-    }
-  })
-
-  .state('tabsController.hEREFORYOU', {
-    url: '/page7',
-    views: {
-      'tab1': {
-        templateUrl: 'templates/hEREFORYOU.html',
-        controller: 'hEREFORYOUCtrl'
-      }
-    }
-  })
-
-  .state('iNVESTORRELATIONS', {
-    url: '/page8',
-    templateUrl: 'templates/iNVESTORRELATIONS.html',
-    controller: 'iNVESTORRELATIONSCtrl'
-  })
-
-  .state('cONTACT', {
-    url: '/page9',
-    templateUrl: 'templates/cONTACT.html',
-    controller: 'cONTACTCtrl'
   })
 
 $urlRouterProvider.otherwise('/page1/page2')
